d6e83fca0428a517bca79afb69d527b85787431d
unknown <rzhossain324@gmail.com>
2021-05-27 17:55:59 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import SearchForm from './SearchForm';
+import './App.css';
 
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+      jokes: [],
+      isFetchingJoke: false
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.searchJokes = this.searchJokes.bind(this);
+  }
+
+  searchJokes(limit = 20) {
+    this.setState({ isFetchingJoke: true });
+    fetch(`https://icanhazdadjoke.com/search?term=${this.state.searchTerm}&limit=${limit}`, 
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        const jokes = json.results;
+        this.setState({
+          jokes,
+          isFetchingJoke: false
+        });
+      });
+  }
+
+  onSearchChange(value) {
+    this.setState({ searchTerm: value });
+  }
+
+  renderJokes() {
+    return (
+      <ul className="jokes-list">
+        {this.state.jokes.map(item => <li key={item.id}>{item.joke}</li>)}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img className="logo" alt="dad-jokes" src="/google-dad-jokes-logo.png" />
+        <SearchForm 
+          onFormSubmit={this.searchJokes}
+          onSearchValueChange={this.onSearchChange}
+          isSearching={this.state.isFetchingJoke}
+          onSingleSearchClick={() => this.searchJokes(1)}
+        />
+        {this.state.isFetchingJoke
+        ? 'Loading joke...'
+        : this.renderJokes()}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
